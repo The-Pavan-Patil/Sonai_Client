6ada584a2ab793f932513d5d535f944223f18a25
pavanpatil <thepavanpatil.official@gmail.com>
2025-10-08 08:26:30 +0530

    fix: update server port to 10000

--- Server/server.js
+++ Server/server.js
@@ -48,7 +48,7 @@ app.get("/", (req, res) => {
 
 
 // Server
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
